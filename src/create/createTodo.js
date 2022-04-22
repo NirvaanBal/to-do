b2843b2eb59deae5e825892ddfad7c2fef5b6603
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-22 15:00:06 +0530

    Create a todos object to hold all tasks based on the relevant project

--- src/create/createTodo.js
+++ src/create/createTodo.js
@@ -1,25 +1,29 @@
 import Todo from '../Todo';
 import parseDate from '../parseDate';
 
+const todos = {};
 const createTodo = () => {
   const form = document.querySelector('#todo-form > form');
 
+  let todo = null;
   form.addEventListener('submit', (e) => {
-    const title = document.querySelector('#title');
-    const description = document.querySelector('#description');
-    const date = document.querySelector('#date');
-    const month = document.querySelector('#month');
-    const project = document.querySelector('#project');
-    const priority = document.querySelector('input[name="priority"]:checked');
+    const title = form.querySelector('#title');
+    const description = form.querySelector('#description');
+    const date = form.querySelector('#date');
+    const month = form.querySelector('#month');
+    const project = form.querySelector('#project');
+    const priority = form.querySelector('input[name="priority"]:checked');
 
     e.preventDefault();
-    let todo = null;
     const dateValue = parseDate(date.value, month.value);
     todo = new Todo(title.value, description.value, dateValue, priority.value);
     if (!!project.value) todo.project = project.value;
-
-    console.log(todo);
+    if (todos[todo.project]) todos[todo.project].push(todo);
+    else {
+      todos[todo.project] = [];
+      todos[todo.project].push(todo);
+    }
   });
 };
 
-export default createTodo;
+export default todos;
