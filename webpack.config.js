120b1257bd153f591f8b29c0c834412cf58ef20b
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-22 10:39:30 +0530

    Add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/i,
-        use: ['sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
