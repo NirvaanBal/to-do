01449d5386eeb88ddd968850057b202c4f5e31aa
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-23 15:52:56 +0530

    Get all todos with projects on data submission

--- src/read/readTodos.js
+++ src/read/readTodos.js
@@ -1,7 +1,33 @@
 const readTodos = (projects) => {
   const rootDiv = document.createElement('div');
   rootDiv.classList.add('projects');
-  rootDiv.textContent = 'All projects!!!';
+
+  for (let project in projects) {
+    const projectDiv = document.createElement('div');
+    projectDiv.classList.add('project');
+    rootDiv.appendChild(projectDiv);
+
+    const heading = document.createElement('h3');
+    heading.textContent = project;
+    projectDiv.appendChild(heading);
+
+    for (let todo of projects[project]) {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo');
+      projectDiv.appendChild(todoDiv);
+
+      const flexDiv = document.createElement('div');
+      todoDiv.appendChild(flexDiv);
+
+      const heading = document.createElement('h4');
+      heading.textContent = todo.title;
+      flexDiv.appendChild(heading);
+
+      const date = document.createElement('p');
+      date.textContent = todo.dueDate;
+      flexDiv.appendChild(date);
+    }
+  }
 
   return rootDiv;
 };
