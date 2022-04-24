1b82dacbb56c342691c8eefb9cc0255b9fbc7e18
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-24 19:09:25 +0530

    Get current day and month on form by default

--- src/layout/form.js
+++ src/layout/form.js
@@ -1,5 +1,8 @@
-const formHTML = `
-    <form>
+const formHTML = () => {
+  const day = new Date().getDate();
+  const month = new Date().getMonth();
+
+  let html = `<form>
         <div class="form-control">
             <label for="title">Task</label>
             <input type="text" name="title" id="title" required />
@@ -16,54 +19,37 @@ const formHTML = `
             <p>Due Date</p>
             <div class="date">
             <label for="date">Date</label>
-            <select name="date" id="date">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
-            </select>
+            <select name="date" id="date">`;
+
+  for (let i = 1; i <= 31; i++) {
+    if (i === day) html += `<option value="${i}" selected>${i}</option>`;
+    else html += `<option value="${i}">${i}</option>`;
+  }
+
+  html += `</select>
             <label for="month">Month</label>
-            <select name="month" id="month">
-                <option value="jan">January</option>
-                <option value="feb">February</option>
-                <option value="mar">March</option>
-                <option value="apr">April</option>
-                <option value="may">May</option>
-                <option value="jun">June</option>
-                <option value="jul">July</option>
-                <option value="aug">August</option>
-                <option value="sep">September</option>
-                <option value="oct">October</option>
-                <option value="nov">November</option>
-                <option value="dec">December</option>
-            </select>
+            <select name="month" id="month">`;
+
+  [
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec',
+  ].forEach((m, index) => {
+    if (index === month) {
+      html += `<option value="${m}" selected>${m.toUpperCase()}</option>`;
+    } else html += `<option value="${m}">${m.toUpperCase()}</option>`;
+  });
+
+  html += `</select>
             </div>
         </div>
         <div class="form-control">
@@ -83,14 +69,15 @@ const formHTML = `
         <div class="form-control">
             <button type="submit" id="to-do-btn">Create</button>
         </div>
-    </form>
-`;
+    </form>`;
 
+  return html;
+};
 const formContainer = document.createElement('div');
 formContainer.setAttribute('id', 'todo-form');
 const heading = document.createElement('h2');
 heading.textContent = 'Create a new task';
 formContainer.appendChild(heading);
-formContainer.insertAdjacentHTML('beforeend', formHTML);
+formContainer.insertAdjacentHTML('beforeend', formHTML());
 
 export default formContainer;
