11b892a6fbdbae17f749066d607fe8b0e239324a
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-25 18:53:48 +0530

    Fix bug related to the months having days less than 31

--- src/utils/parseDate.js
+++ src/utils/parseDate.js
@@ -1,4 +1,16 @@
 const parseDate = (date, month) => {
+  if (
+    date === '31' &&
+    (month === 'apr' || month === 'jun' || month === 'sep' || month === 'nov')
+  ) {
+    date = '30';
+  }
+
+  if ((date === '29' || date === '30' || date === '31') && month === 'feb') {
+    date = '28';
+    if (new Date().getFullYear() % 4 === 0) date = '29';
+  }
+
   return `${month} ${date}`;
 };
 
