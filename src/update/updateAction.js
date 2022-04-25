77e4e0dd74651c476a714544a639e1c9b0163b55
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-25 18:41:50 +0530

    Update localstorage on changing priority, status of or deleting a task

--- src/update/updateAction.js
+++ src/update/updateAction.js
@@ -8,19 +8,17 @@ const updateAction = (element, projects, action) => {
       // else task.changePriority('low');
       if (task.priority === 'low') task.priority = 'high';
       else task.priority = 'low';
-      return;
     } else if (action === 'completed' && task.id === taskId) {
       // task.isComplete();
       task.completed = !task.completed;
-      return;
     } else if (action === 'delete' && task.id === taskId) {
       projects[project] = [...projects[project]].filter((task) => {
         return task.id !== taskId;
       });
       if (projects[project].length === 0) delete projects[project];
-      return;
     }
   }
+  localStorage.setItem('projects', JSON.stringify(projects));
 };
 
 export default updateAction;
